eee3a731d6a9d12ae54162e74b9bb388581762d4
HappyFishhhy <s1171148493@gmail.com>
2018-01-03 23:09:03 +0800

    final edition

--- app.py
+++ app.py
@@ -7,35 +7,127 @@ from flask import Flask, request, send_file
 from fsm import TocMachine
 
 
-API_TOKEN = 'Your Telegram API Token'
-WEBHOOK_URL = 'Your Webhook URL'
+API_TOKEN = '523271669:AAGko0YjqKeniLCUiMBtW4jZTYkTBT3rNX4'
+WEBHOOK_URL = 'https://08f6994f.ngrok.io/hook'
 
 app = Flask(__name__)
 bot = telegram.Bot(token=API_TOKEN)
 machine = TocMachine(
     states=[
         'user',
-        'state1',
-        'state2'
+        'helmet',
+        'motogp',
+        'clothes',
+        'shockabsorber',
+        'below2000',
+        'from2000to8000',
+        'above8000',
+        'sol1',
+        'sol2',
+        'zeus1',
+        'zeus2',
+        'thh1',
+        'thh2',
+        'agv'
     ],
     transitions=[
         {
             'trigger': 'advance',
             'source': 'user',
-            'dest': 'state1',
-            'conditions': 'is_going_to_state1'
+            'dest': 'helmet',
+            'conditions': 'is_going_to_helmet'
         },
         {
             'trigger': 'advance',
             'source': 'user',
-            'dest': 'state2',
-            'conditions': 'is_going_to_state2'
+            'dest': 'motogp',
+            'conditions': 'is_going_to_motogp'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'user',
+            'dest': 'clothes',
+            'conditions': 'is_going_to_clothes'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'user',
+            'dest': 'shockabsorber',
+            'conditions': 'is_going_to_shockabsorber'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'helmet',
+            'dest': 'below2000',
+            'conditions': 'is_going_to_below2000'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'helmet',
+            'dest': 'from2000to8000',
+            'conditions': 'is_going_to_from2000to8000'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'helmet',
+            'dest': 'above8000',
+            'conditions': 'is_going_to_above8000'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'below2000',
+            'dest': 'sol1',
+            'conditions': 'is_going_to_sol1'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'from2000to8000',
+            'dest': 'sol2',
+            'conditions': 'is_going_to_sol2'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'below2000',
+            'dest': 'zeus1',
+            'conditions': 'is_going_to_zeus1'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'from2000to8000',
+            'dest': 'zeus2',
+            'conditions': 'is_going_to_zeus2'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'below2000',
+            'dest': 'thh1',
+            'conditions': 'is_going_to_thh1'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'from2000to8000',
+            'dest': 'thh2',
+            'conditions': 'is_going_to_thh2'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'above8000',
+            'dest': 'agv',
+            'conditions': 'is_going_to_agv'
         },
         {
             'trigger': 'go_back',
             'source': [
-                'state1',
-                'state2'
+                'helmet',
+                'motogp',
+                'clothes',
+                'shockabsorber',
+                'below2000',
+                'from2000to8000',
+                'above8000',
+                'helmetbrand1',
+                'helmetbrand2',
+                'helmetbrand3'
             ],
             'dest': 'user'
         }
--- fsm.py
+++ fsm.py
@@ -8,24 +8,164 @@ class TocMachine(GraphMachine):
             **machine_configs
         )
 
-    def is_going_to_state1(self, update):
+    def is_going_to_helmet(self, update):
         text = update.message.text
-        return text.lower() == 'go to state1'
+        return text.lower() == 'helmet'
 
-    def is_going_to_state2(self, update):
+    def is_going_to_motogp(self, update):
         text = update.message.text
-        return text.lower() == 'go to state2'
+        return text.lower() == 'motogp'
 
-    def on_enter_state1(self, update):
-        update.message.reply_text("I'm entering state1")
+    def is_going_to_clothes(self, update):
+        text = update.message.text
+        return text.lower() == 'clothes'
+
+    def is_going_to_shockabsorber(self, update):
+        text = update.message.text
+        return text.lower() == 'shockabsorber'
+
+    def is_going_to_below2000(self, update):
+        text = update.message.text
+        return text.lower() == 'below2000'
+
+    def is_going_to_from2000to8000(self, update):
+        text = update.message.text
+        return text.lower() == 'from2000to8000'
+
+    def is_going_to_above8000(self, update):
+        text = update.message.text
+        return text.lower() == 'above8000'
+
+    def is_going_to_sol1(self, update):
+        text = update.message.text
+        return text.lower() == 'sol'
+
+    def is_going_to_sol2(self, update):
+        text = update.message.text
+        return text.lower() == 'sol'
+
+    def is_going_to_zeus1(self, update):
+        text = update.message.text
+        return text.lower() == 'zeus'
+
+    def is_going_to_zeus2(self, update):
+        text = update.message.text
+        return text.lower() == 'zeus'
+
+    def is_going_to_thh1(self, update):
+        text = update.message.text
+        return text.lower() == 'thh'
+
+    def is_going_to_thh2(self, update):
+        text = update.message.text
+        return text.lower() == 'thh'
+
+    def is_going_to_agv(self, update):
+        text = update.message.text
+        return text.lower() == 'agv'
+
+    def on_enter_helmet(self, update):
+        update.message.reply_text("ok,how much you could pay?\nbelow2000, from2000to8000, above8000")
+        """self.go_back(update)"""
+
+    def on_exit_helmet(self, update):
+        print('Leaving helmet')
+
+    def on_enter_motogp(self, update):
+        update.message.reply_text("what information do you want to know? personal champion or team champion?")
         self.go_back(update)
 
-    def on_exit_state1(self, update):
-        print('Leaving state1')
+    def on_exit_motogp(self, update):
+        print('Leaving motogp')
 
-    def on_enter_state2(self, update):
-        update.message.reply_text("I'm entering state2")
+
+    def on_enter_clothes(self, update):
+        update.message.reply_text("ok,here are some famous brands around the world\nDainese, Alpinestar...\nWhich one do you want to understand?")
         self.go_back(update)
 
-    def on_exit_state2(self, update):
-        print('Leaving state2')
+    def on_exit_clothes(self, update):
+        print('Leaving clothes')
+
+    def on_enter_shockabsorber(self, update):
+        update.message.reply_text("ok,here are some famous brands in Taiwan\nGJMS, Windwolf, rpm, msp...\nwhich one do you want to understand?")
+        self.go_back(update)
+
+    def on_exit_shockabsorber(self, update):
+        print('Leaving shockabsorber')
+
+    def on_enter_below2000(self, update):
+        update.message.reply_text("ok, which brand you prefer?\nsol, zeus,or thh ?")
+        """self.go_back(update)"""
+
+    def on_exit_below2000(self, update):
+        print('Leaving helmet')
+
+    def on_enter_from2000to8000(self, update):
+        update.message.reply_text("ok, which brand you prefer?\nsol, zeus,or thh ?")
+        """self.go_back(update)"""
+
+    def on_exit_from2000to8000(self, update):
+        print('Leaving helmet')
+
+    def on_enter_above8000(self, update):
+        update.message.reply_text("ok, which brand you prefer? \nagv ?")
+        """self.go_back(update)"""
+
+    def on_exit_above8000(self, update):
+        print('Leaving helmet')
+
+    def on_enter_sol1(self, update):
+        update.message.reply_text("ok,it's your sol")
+        update.message.reply_photo(open("img/sol1.jpeg","rb"))
+        self.go_back(update)
+
+    def on_exit_sol1(self, update):
+        print('Leaving helmet')
+
+    def on_enter_sol2(self, update):
+        update.message.reply_text("ok,it's your sol")
+        update.message.reply_photo(open("img/sol2.jpg","rb"))
+        self.go_back(update)
+
+    def on_exit_sol2(self, update):
+        print('Leaving helmet')
+
+    def on_enter_zeus1(self, update):
+        update.message.reply_text("ok,it's your zeus")
+        update.message.reply_photo(open("img/zeus1.jpg","rb"))
+        self.go_back(update)
+
+    def on_exit_zeus1(self, update):
+        print('Leaving helmet')
+
+    def on_enter_zeus2(self, update):
+        update.message.reply_text("ok,it's your zeus")
+        update.message.reply_photo(open("img/zeus2.jpg","rb"))
+        self.go_back(update)
+
+    def on_exit_zeus2(self, update):
+        print('Leaving helmet')
+
+    def on_enter_thh1(self, update):
+        update.message.reply_text("ok,it's your thh")
+        update.message.reply_photo(open("img/thh1.jpg","rb"))
+        self.go_back(update)
+
+    def on_exit_thh1(self, update):
+        print('Leaving helmet')
+
+    def on_enter_thh2(self, update):
+        update.message.reply_text("ok,it's your thh")
+        update.message.reply_photo(open("img/thh2.jpg","rb"))
+        self.go_back(update)
+
+    def on_exit_thh2(self, update):
+        print('Leaving helmet')
+
+    def on_enter_agv(self, update):
+        update.message.reply_text("ok,it's your agv pista gp r?")
+        update.message.reply_photo(open("img/agv.jpg","rb"))
+        self.go_back(update)
+
+    def on_exit_agv(self, update):
+        print('Leaving helmet')
